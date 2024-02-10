6d02bbddb840119d25856d301d1ae180f7624c32
Career-Nandish <career.nandish@gmail.com>
2024-02-10 22:24:48 +1100

    hackerrank chanllenges till 27

--- Hackerrank Challenges/hackerrank_challenges.sql
+++ Hackerrank Challenges/hackerrank_challenges.sql
@@ -406,3 +406,101 @@ SELECT CEIL(AVG(SALARY) - AVG(CAST(REPLACE(CAST(SALARY AS CHAR), '0','') AS SIGN
 FROM EMPLOYEES
 
 
+## 25. Type of Triangle
+
+
+/*
+https://www.hackerrank.com/challenges/what-type-of-triangle/problem
+
+Write a query identifying the type of each record in the TRIANGLES 
+table using its three side lengths.
+*/
+
+
+SELECT CASE
+            WHEN (A + B <= C) OR (A + C <= B) OR (B + C <= A) THEN 'Not A Triangle'
+            WHEN (A = B AND B = C) THEN 'Equilateral'
+            WHEN (A = B AND B != C) OR (A != B AND B = C) OR (A = C AND C != B) THEN 'Isosceles'
+            WHEN (A != B AND B != C AND A != C) THEN 'Scalene'
+       END
+FROM TRIANGLES
+
+
+## 26. The PADS
+
+/*
+https://www.hackerrank.com/challenges/the-pads/problem
+
+Query an alphabetically ordered list of all names in OCCUPATIONS, 
+immediately followed by the first letter of each profession as a 
+parenthetical (i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and 
+ASingerName(S).
+
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort 
+the occurrences in ascending order, and output them in the following 
+format:
+
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation 
+in OCCUPATIONS and [occupation] is the lowercase occupation name. If 
+more than one Occupation has the same [occupation_count], they should 
+be ordered alphabetically.
+*/
+
+
+SELECT CONCAT(Name, '(', UPPER(LEFT(Occupation, 1)), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', lower(Occupation),'s.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*), Occupation
+
+
+## 27. Weather Observation Station 18
+
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-18/problem
+
+Consider P1(a, b) and P2(c, d) to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to 
+a scale of  decimal places.
+*/
+
+
+SELECT ROUND(ABS(a - c) + ABS(b - d), 4)
+FROM (
+    SELECT MIN(LAT_N) AS a,
+           MIN(LONG_W) AS b,
+           MAX(LAT_N) AS c,
+           MAX(LONG_W) AS d
+    FROM STATION
+    ) AS subq
+
+
+## 28.
+
+/*
+
+*/
+
+
+## 29.
+
+/*
+
+*/
+
+
+## 30.
+
+/*
+
+*/
