affff50095bb4fe361ad8057f1705ee091dedadc
Career-Nandish <career.nandish@gmail.com>
2024-02-12 21:25:21 +1100

    Hackerrank challenge 28 added

--- Hackerrank Challenges/hackerrank_challenges.sql
+++ Hackerrank Challenges/hackerrank_challenges.sql
@@ -485,12 +485,37 @@ FROM (
     ) AS subq
 
 
-## 28.
+## 28. New Companies
 
 /*
+https://www.hackerrank.com/challenges/the-company/problem
 
+Amber's conglomerate corporation just acquired some new companies. 
+Each of the companies follows this hierarchy:
+
+Founder -> Lead Manager -> Senior Manager -> Manager -> Employee
+
+Given the table schemas below, write a query to print the company_code, 
+founder name, total number of lead managers, total number of senior 
+managers, total number of managers, and total number of employees. 
+Order your output by ascending company_code.
 */
 
+
+SELECT c.company_code, 
+       c.founder, 
+       COUNT(DISTINCT e.lead_manager_code),
+       COUNT(DISTINCT e.senior_manager_code),
+       COUNT(DISTINCT e.manager_code),
+       COUNT(DISTINCT e.employee_code)
+FROM Company c
+JOIN Employee e
+    ON c.company_code = e.company_code
+GROUP BY c.founder, c.company_code
+ORDER BY c.company_code
+
+
+/*OR*/
 
 ## 29.
 
