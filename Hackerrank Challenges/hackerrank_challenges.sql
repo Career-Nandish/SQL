3981b6a5b11c304b6ccbaa0515f4abe49e38252b
Career-Nandish <career.nandish@gmail.com>
2024-02-14 00:41:05 +1100

    Hackerrank challenge 33 added

--- Hackerrank Challenges/hackerrank_challenges.sql
+++ Hackerrank Challenges/hackerrank_challenges.sql
@@ -625,3 +625,33 @@ FROM Students s
 LEFT JOIN Grades g
     ON s.Marks >= g.Min_Mark AND s.Marks <= g.Max_Mark
 ORDER BY g.Grade DESC, s.Name ASC, s.Marks ASC
+
+
+## 33. Top Competitors
+
+/*
+URL - https://www.hackerrank.com/challenges/full-score/problem
+
+Julia just finished conducting a coding contest, and she
+needs your help assembling the leaderboard! Write a 
+query to print the respective hacker_id and name of 
+hackers who achieved full scores for more than one 
+challenge. Order your output in descending order by the
+total number of challenges in which the hacker earned a
+full score. If more than one hacker received full scores
+in same number of challenges, then sort them by 
+ascending hacker_id.
+*/
+
+
+SELECT h.hacker_id, h.name
+FROM hackers h
+JOIN submissions s
+    ON h.hacker_id = s.hacker_id
+JOIN challenges c
+    ON s.challenge_id = c.challenge_id
+JOIN difficulty d
+    ON c.difficulty_level = d.difficulty_level AND s.score = d.score
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(s.challenge_id) > 1
+ORDER BY COUNT(s.challenge_id) DESC, h.hacker_id
