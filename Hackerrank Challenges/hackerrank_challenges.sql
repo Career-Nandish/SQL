4776a2680e405b0c19d4970828f59cf0544482ff
Career-Nandish <career.nandish@gmail.com>
2024-02-20 22:46:10 +1100

    Hackerrank challenge 37 added

--- Hackerrank Challenges/hackerrank_challenges.sql
+++ Hackerrank Challenges/hackerrank_challenges.sql
@@ -752,3 +752,32 @@ JOIN (
 ) AS SUBQ2
     ON h.hacker_id = SUBQ2.hacker_id
 ORDER BY SUBQ2.tot_max_score DESC, h.hacker_id
+
+
+## 37. Placements
+
+/*
+URL - https://www.hackerrank.com/challenges/placements/problem
+
+You are given three tables: Students, Friends and Packages. Students 
+contains two columns: ID and Name. Friends contains two columns: ID and 
+Friend_ID (ID of the ONLY best friend). Packages contains two columns: 
+ID and Salary (offered salary in $ thousands per month).
+
+Write a query to output the names of those students whose best friends 
+got offered a higher salary than them. Names must be ordered by the 
+salary amount offered to the best friends. It is guaranteed that no two 
+students got same salary offer.
+*/
+
+
+SELECT s.name
+FROM students s
+JOIN friends f
+    ON s.id = f.id
+JOIN packages p
+    ON f.id = p.id
+JOIN packages fp
+    ON fp.id = f.friend_id
+WHERE p.salary < fp.salary
+ORDER BY fp.salary
