661014e4c4565dd899f3ea38288d77011f86ae86
Career-Nandish <career.nandish@gmail.com>
2024-03-02 20:03:49 +1100

    Hackerrank challenge 44 added

--- Hackerrank Challenges/hackerrank_challenges.sql
+++ Hackerrank Challenges/hackerrank_challenges.sql
@@ -975,3 +975,30 @@ FROM (
 ) AS subq
 GROUP BY subq.diff
 ORDER BY ed - sd, sd
+
+
+## 44. Binary Tree Nodes
+
+/*
+URL - https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+
+You are given a table, BST, containing two columns: N and P, where N 
+represents the value of a node in Binary Tree, and P is the parent of N.
+
+Write a query to find the node type of Binary Tree ordered by the value 
+of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+
+
+SELECT N, 
+       CASE
+            WHEN P IS NULL THEN 'Root'
+            WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+            ELSE 'Leaf'
+       END AS s
+FROM BST
+ORDER BY N
